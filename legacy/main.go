bf37a3a6afc4026e0f7d433018f1c4b9259aad56
Kaito-Dogi <kendobu0405@gmail.com>
2025-11-03 19:10:59 +0900

    chore: legacy ディレクトリにコピーを置いた

---
+++ legacy/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/modelcontextprotocol/go-sdk/mcp"
+)
+
+type Input struct {
+	Name string `json:"name" jsonschema:"the name of the person to greet"`
+}
+
+type Output struct {
+	Greeting string `json:"greeting" jsonschema:"the greeting to tell to the user"`
+}
+
+func SayHello(
+	ctx context.Context,
+	req *mcp.CallToolRequest,
+	input Input,
+) (
+	*mcp.CallToolResult,
+	Output,
+	error,
+) {
+	return nil, Output{Greeting: "Hello, " + input.Name + "!"}, nil
+}
+
+func main() {
+	log.Println(`あああ: start main`)
+
+	// MCP サーバーを作成
+	server := mcp.NewServer(&mcp.Implementation{Name: "mcp-server-play-sound", Version: "v0.0.1"}, nil)
+
+	log.Println(`あああ: created a mcp server`)
+
+	// ツールを登録
+	mcp.AddTool(server, &mcp.Tool{Name: "greet", Description: "Say Hello"}, SayHello)
+
+	log.Println(`あああ: added a tool`)
+
+	if err := server.Run(context.Background(), &mcp.StdioTransport{}); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(`あああ: running mcp server...`)
+}
